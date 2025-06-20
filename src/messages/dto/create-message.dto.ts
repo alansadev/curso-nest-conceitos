843800f2bdff9aefbd6ec467082fbb352adf7ff3
Alan Sá <alkinsret@gmail.com>
2025-06-20 11:20:35 -0300

    feat: dto validation

--- src/messages/dto/create-message.dto.ts
+++ src/messages/dto/create-message.dto.ts
@@ -1,5 +1,21 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreateMessageDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  @MaxLength(255)
   readonly text: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(255)
   readonly sender: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(255)
   readonly receiver: string;
 }
