ca970f06db331d034a8eebaed8dbe9004b7babf6
Alan Sá <alkinsret@gmail.com>
2025-06-20 19:06:41 -0300

    feat: first typeorm entity

--- src/messages/entities/message.entity.ts
+++ src/messages/entities/message.entity.ts
@@ -1,8 +1,34 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('messages')
 export class MessageEntity {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'varchar', length: 255 })
   text: string;
+
+  @Column({ type: 'varchar', length: 50 })
   sender: string;
+
+  @Column({ type: 'varchar', length: 50 })
   receiver: string;
+
+  @Column({ default: false })
   read: boolean;
+
+  @Column()
   date: Date;
+
+  @CreateDateColumn()
+  createdAt?: Date;
+
+  @UpdateDateColumn()
+  updatedAt?: Date;
 }
