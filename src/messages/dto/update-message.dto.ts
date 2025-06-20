ba47435366f7b15d6d53a4f3963d81e609aac317
Alan Sá <alkinsret@gmail.com>
2025-06-20 11:25:20 -0300

    feat: dto mapped-type

--- src/messages/dto/update-message.dto.ts
+++ src/messages/dto/update-message.dto.ts
@@ -1,5 +1,22 @@
-export class UpdateMessageDto {
-  readonly text?: string;
-  readonly sender?: string;
-  readonly receiver?: string;
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateMessageDto } from './create-message.dto';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class UpdateMessageDto extends PartialType(CreateMessageDto) {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  @MaxLength(255)
+  readonly text: string;
+
+  @IsBoolean()
+  @IsOptional()
+  readonly read?: boolean;
 }
